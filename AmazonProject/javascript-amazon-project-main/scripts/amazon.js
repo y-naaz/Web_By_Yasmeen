07f74b32a11c6e90c57a6967f51b75fb8f1667ae
YASMEEN <yasmeennaaz@gmail.com>
2024-11-16 10:05:33 +0530

    used product.js file

--- AmazonProject/javascript-amazon-project-main/scripts/amazon.js
+++ AmazonProject/javascript-amazon-project-main/scripts/amazon.js
@@ -1,31 +1,4 @@
-const products  = [{
-    image:"images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating:{
-        stars:4.5,
-        count : 87
-    },
-    price_cents: 1090
-},
-{
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name:'Intermediate Size Basketball',
-    rating :{
-        stars:4,
-        count:127
-    },
-    price_cents:2095
-},
-{
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        stars:4.5,
-        count : 56
-    },
-    price_cents:799
-}
-];
+//Now I have deleted the product array and all the product's javascript code is contained in product.js file
 //loop through the list of arrays
 //to add some HTML to the list of products
 let productHTML='';
